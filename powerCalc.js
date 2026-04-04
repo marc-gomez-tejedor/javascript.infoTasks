aafdc2cff81f06d07298c0e2ee5399bf28af8723
Marc <marcgomeztejedor@gmail.com>
2026-04-04 02:47:30 +0200

    extendable calculator, super proud

---
+++ powerCalc.js
@@ -0,0 +1,34 @@
+"use strict"
+function Calculator() {
+  this.calculate = function (operationString) {
+    let arrOperationSplit = [];
+    arrOperationSplit = operationString.split(" ");
+    let operandA = +arrOperationSplit[0];
+    let operandB = +arrOperationSplit[2];
+    let charOperationTypeKey = arrOperationSplit[1];
+    return this[charOperationTypeKey](operandA, operandB);
+  };
+  this["+"] = function (a, b) {
+    return a + b;
+  }
+  this["-"] = function (a, b) {
+    return a - b;
+  }
+  this.addMethod = function(charOperationTypeKey, methodToAdd) {
+    this[charOperationTypeKey] = methodToAdd;
+  };
+}
+
+let basicCalculator = new Calculator;
+
+alert( basicCalculator.calculate("3 + 7") ); // 10
+alert( basicCalculator.calculate("23 - 17") ); // 6
+
+let extendableCalculator = new Calculator;
+extendableCalculator.addMethod("*", (a, b) => a * b);
+extendableCalculator.addMethod("/", (a, b) => a / b);
+extendableCalculator.addMethod("**", (a, b) => a ** b);
+
+let result = extendableCalculator.calculate("2 ** 3");
+alert( result ); // 8
+
